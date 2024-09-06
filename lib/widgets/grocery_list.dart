b8b8fd19d45504a3ce448b726bfe76f37caafdb4
Arthur Cardoso <arthurcardoso2005@gmail.com>
2024-09-05 21:34:19 -0300

    course finished

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/grocery_model.dart';
 import 'package:shopping_list_app/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,7 +13,97 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryModel> _groceryItems = [];
+  List<GroceryModel> _groceryItems = [];
+  late Future<List<GroceryModel>> _loadedItems;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadedItems = _loadItems();
+  }
+
+  Future<List<GroceryModel>> _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-b7f8f-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      throw Exception('Failed to fetch data');
+    }
+
+    /// Esse "if" serve para que não dê ruim no aplicativo para o caso de não ter
+    /// ao menos 1 item na lista. Pois, se não tiver nenhum, ele retorna "null" o que
+    /// dá problema, já que ele vai tentar colocar "null" como um Map<String,dynamic>
+    /// Nesse caso, está como "null", entre aspas, porque é a String que o json retorna
+    if (response.body == 'null') {
+      return [];
+    }
+    final Map<String, dynamic> listData = jsonDecode(response.body);
+    final List<GroceryModel> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      loadedItems.add(
+        GroceryModel(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    return loadedItems;
+  }
+
+  /*void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-b7f8f-default-rtdb.firebaseio.com', 'shopping-list.json');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data. Please, try again later.';
+        });
+      }
+
+      /// Esse "if" serve para que não dê ruim no aplicativo para o caso de não ter
+      /// ao menos 1 item na lista. Pois, se não tiver nenhum, ele retorna "null" o que
+      /// dá problema, já que ele vai tentar colocar "null" como um Map<String,dynamic>
+      /// Nesse caso, está como "null", entre aspas, porque é a String que o json retorna
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      final Map<String, dynamic> listData = jsonDecode(response.body);
+      final List<GroceryModel> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(
+          GroceryModel(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong. Please, try again later.';
+      });
+    }
+  } */
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryModel>(
@@ -19,6 +112,7 @@ class _GroceryListState extends State<GroceryList> {
       ),
     );
 
+    _loadItems();
     if (newItem == null) {
       return;
     }
@@ -26,6 +120,24 @@ class _GroceryListState extends State<GroceryList> {
     setState(() {
       _groceryItems.add(newItem);
     });
+  }
+
+  void _removeItem(GroceryModel item) async {
+    final index = _groceryItems.indexOf(item);
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    final url = Uri.https('flutter-prep-b7f8f-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      // Se der erro, adiciona novamente o item na lista
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
@@ -37,30 +149,53 @@ class _GroceryListState extends State<GroceryList> {
           IconButton(onPressed: _addItem, icon: const Icon(Icons.add)),
         ],
       ),
-      body: _groceryItems.isEmpty
-          ? const Center(
-              child: Text('Click on (+) button to add a new item'),
-            )
-          : ListView.builder(
-              itemCount: _groceryItems.length,
-              itemBuilder: (context, index) => Dismissible(
-                direction: DismissDirection.endToStart,
-                onDismissed: (direction) {
-                  _groceryItems.remove(_groceryItems[index]);
-                },
-                key: ValueKey(_groceryItems[index].id),
-                child: ListTile(
-                  leading: Container(
-                    color: _groceryItems[index].category.color,
-                    width: 20,
-                    height: 20,
-                  ),
-                  title: Text(_groceryItems[index].name),
-                  subtitle: const Text('Swipe to the left to remove the item', style: TextStyle(fontSize: 12),),
-                  trailing: Text(_groceryItems[index].quantity.toString(), style: const TextStyle(fontSize: 14),),
+      body: FutureBuilder(
+        // Usa o "_loadedItems" ao inves da função "_loadItems" para que a função não seja executada
+        // toda vez que o método "build" seja chamado, que ai ele só é executado no "initState"
+        future: _loadedItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else if (snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text('No Items added yet.'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length, // Antes era _groceryItems.length
+            itemBuilder: (context, index) => Dismissible(
+              direction: DismissDirection.endToStart,
+              onDismissed: (direction) {
+                _removeItem(snapshot.data![index]);
+              },
+              key: ValueKey(snapshot.data![index].id),
+              child: ListTile(
+                leading: Container(
+                  color: snapshot.data![index].category.color,
+                  width: 20,
+                  height: 20,
+                ),
+                title: Text(snapshot.data![index].name),
+                subtitle: const Text(
+                  'Swipe to the left to remove the item',
+                  style: TextStyle(fontSize: 12),
+                ),
+                trailing: Text(
+                  snapshot.data![index].quantity.toString(),
+                  style: const TextStyle(fontSize: 14),
                 ),
               ),
             ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category_model.dart';
 import 'package:shopping_list_app/models/grocery_model.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -15,19 +18,64 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false; // Só para saber se está enviando a informação
 
   // Método para salvar o item
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https('flutter-prep-b7f8f-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
+
+      response.body;
+      response.statusCode;
+      final Map<String, dynamic> resData = jsonDecode(response.body);
+
+      /// Para que não dê um aviso de que não é para utilizar "context" em uma função assíncrona
+      /// Pode checar colocando esse if com o argumento "!context.mounted"
+      /// Para assegurar ao flutter que não estaremos referenciando um context desatualizado,
+      /// ou que não faz mais parte da tela.
+      /// Basicamente, se o contexto não estiver ativo "mounted" retorna sem executar
+      /// o "Navigator.of(context).pop()". Ou seja, se o Widget que esse contexto pertence
+      /// não faz mais parte da tela, retorna e não executa o código posterior
+      if (!context.mounted) {
+        return;
+      }
+
+      /// Por que aparece o "Don't use 'BuildContext's across async gaps"?
+      /// Porque, quando tem o "await" o Flutter fica sem saber se o "context" ainda será
+      /// o mesmo quando a operação for finalizada, e que ele pode estar então se referenciando
+      /// a um Widget que não existe mais.
+      Navigator.of(context).pop(GroceryModel(
+        id: resData['name'],
+        name: _enteredName,
+        quantity: _enteredQuantity,
+        category: _selectedCategory,
+      ));
+      /*Navigator.of(context).pop(
         GroceryModel(
           id: DateTime.now().toString(),
           name: _enteredName,
           quantity: _enteredQuantity,
           category: _selectedCategory,
         ),
-      );
+      );*/
     }
   }
 
@@ -127,14 +175,24 @@ class _NewItemState extends State<NewItem> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TextButton(
-                      onPressed: () {
-                        _formKey.currentState!
-                            .reset(); // Faz com que resete todos os valores entrados
-                      },
+                      onPressed: _isSending
+                          ? null
+                          : () {
+                              _formKey.currentState!
+                                  .reset(); // Faz com que resete todos os valores entrados
+                            },
                       child: const Text('Reset'),
                     ),
                     ElevatedButton(
-                        onPressed: _saveItem, child: const Text('Add Item'))
+                      onPressed: _isSending ? null : _saveItem,
+                      child: _isSending
+                          ? const SizedBox(
+                              height: 16,
+                              width: 16,
+                              child: CircularProgressIndicator(),
+                            )
+                          : const Text('Add Item'),
+                    ),
                   ],
                 ),
               ],
